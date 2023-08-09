0d43be50c14671540f335d74e512d1d29ffb437f
Elahe Mahmoodpour <elahe.mahmoodpour@gmail.com>
2023-08-09 20:18:02 +0300

    refactor: changed the index.tsx to be up to date and compatible with react 18

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,19 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
+// import "./index.css";
 import App from "./App";
+import { createRoot } from "react-dom/client";
+
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 
-ReactDOM.render(
+const container = document.getElementById("root");
+const root = createRoot(container!);
+root.render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
