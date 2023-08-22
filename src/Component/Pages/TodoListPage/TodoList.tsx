37177ca4733249d24fd8d02adf1a8b01681900d4
Elahe Mahmoodpour <elahe.mahmoodpour@gmail.com>
2023-08-22 09:24:20 +0300

    Refactor: changed the style of modal

--- src/Component/Pages/TodoListPage/TodoList.tsx
+++ src/Component/Pages/TodoListPage/TodoList.tsx
@@ -84,11 +84,10 @@ const TodoList: React.FC<ITodoList> = ({
                                 top: "50%",
                                 left: "50%",
                                 transform: "translate(-50%, -50%)",
-                                width: 400,
-                                bgcolor: "background.paper",
-                                border: "2px solid #000",
-                                boxShadow: 24,
-                                p: 4,
+                                width: 300,
+                                bgcolor: "ButtonFace",
+                                boxShadow: 15,
+                                p: 3.5,
                                 textAlign: "center",
                               }}
                             >
